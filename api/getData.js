eb2dee61dae906b1e3f0e2265b2aa0439d3dd0b9
mikulilin <3095139889@qq.com>
2025-09-21 17:16:17 +0800

    Update getData.js

--- api/getData.js
+++ api/getData.js
@@ -3,7 +3,7 @@
 import { OneNetApi } from '../sdk.js';  // 确保 sdk.js 支持模块化，否则直接在 index.html 引入
 
 const apiKey = 'tIEbQvV3+u9IZvVwlIZZtZ10F8QmEeU12Koqjx30StqQBBVXMtkqdmgDY4+mCZBw';  // 替换你的 Master-APIKey
-const deviceId = 722413660;          // 替换你的设备ID
+const deviceId = 2469496646;          // 替换你的设备ID
 const api = new OneNetApi(apiKey);
 
 export async function getGPS() {
