011ff7c5526a58970426cac438c5cfc4ccf8da0d
mikulilin <3095139889@qq.com>
2025-09-21 15:10:11 +0800

    Update getData.js

--- api/getData.js
+++ api/getData.js
@@ -1,18 +1,20 @@
 export default async function handler(req, res) {
-  const deviceId = "2469496646";      // 替换为你的 OneNET 设备ID
-  const apiKey = "version=2018-10-31&res=products%2FdyLIMxujKq%2Fdevices%2Fnbiot&et=1758183737&method=md5&sign=bbDmpi9BkOWjLa6HWBlf1g%3D%3D";        // 替换为你的 OneNET API Key
+  const deviceId = "2469496646";  // 替换为你的设备ID
+  const apiKey = "version=2018-10-31&res=products%2FdyLIMxujKq%2Fdevices%2Fnbiot&et=9998183737&method=md5&sign=mNhuxhUEoHGTLQegr2Wbjw%3D%3D"; // 替换为你的 API Key
 
+  // 查询历史数据点
   const url = `https://api.heclouds.com/devices/${deviceId}/datapoint/history-datapoints?datastream_id=GPS`;
 
   try {
     const response = await fetch(url, {
-      headers: {
-        "api-key": apiKey
-      }
+      headers: { "api-key": apiKey }
     });
+
     if (!response.ok) throw new Error(`HTTP ${response.status}`);
+
     const data = await response.json();
     res.status(200).json(data);
+
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
