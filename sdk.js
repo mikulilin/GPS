10ee9e6b3c49afb1d7c61b3c5eb84cdc2feac946
mikulilin <3095139889@qq.com>
2025-09-22 08:50:43 +0800

    Update sdk.js

--- sdk.js
+++ sdk.js
@@ -1,11 +1,4 @@
-/**
- * OneNet JS SDK for Browser
- * 注意：使用浏览器端 SDK 会暴露 API Key，仅实验或内部使用
- * 支持模块化导入
- */
-
-(function(global){
-
+(function(){
     var count = 1;
     var API_HOST = 'http://open.iot.10086.cn';
     function noop(){}
@@ -16,40 +9,43 @@
             opts = {};
         }
         if (!opts) opts = {};
+
         var prefix = opts.prefix || 'callback';
         var id = opts.name || (prefix + (count++));
         var param = opts.param || 'callback';
         var timeout = null != opts.timeout ? opts.timeout : 60000;
+        var enc = encodeURIComponent;
         var target = document.getElementsByTagName('script')[0] || document.head;
-        var script;
-        var timer;
+        var script, timer;
 
-        if(timeout){
+        if (timeout) {
             timer = setTimeout(function(){
                 cleanup();
-                if(fn) fn(new Error('Timeout'));
+                if (fn) fn(new Error('Timeout'));
             }, timeout);
         }
 
         function cleanup(){
-            if(script.parentNode) script.parentNode.removeChild(script);
+            if (script.parentNode) script.parentNode.removeChild(script);
             window[id] = noop;
-            if(timer) clearTimeout(timer);
+            if (timer) clearTimeout(timer);
         }
 
         window[id] = function(data){
             cleanup();
-            if(fn) fn(null, data);
+            if (fn) fn(null, data);
         };
 
-        url += (~url.indexOf('?') ? '&' : '?') + param + '=' + encodeURIComponent(id) + '&_=' + (new Date()).getTime();
+        url += (~url.indexOf('?') ? '&' : '?') + param + '=' + enc(id) + '&_=' + (new Date()).getTime();
         url = url.replace('?&', '?');
 
         script = document.createElement('script');
         script.src = url;
         target.parentNode.insertBefore(script, target);
 
-        return function cancel(){ cleanup(); };
+        return function cancel(){
+            cleanup();
+        };
     }
 
     function OneNetApi(apiKey){
@@ -60,27 +56,17 @@
     }
 
     OneNetApi.prototype = {
-        // 读取设备多个数据流（只保留 GPS）
         getDataStreams: function(deviceId){
             var doneCallBack;
-            jsonp(API_HOST + '/api/jsonpresend?key=' + this._apiKey + '&method=GET&uri=devices/' + deviceId + '/datastreams', function(error, res){
+            jsonp(API_HOST + '/api/jsonpresend?key='+this._apiKey+'&method=GET&uri=devices/'+deviceId+'/datastreams', function(error, res){
                 if(error || !res.hasOwnProperty('data')){
                     res = { errno: 100, error: 'timeout' };
-                } else {
-                    // 过滤只保留 GPS 数据流
-                    res.data = res.data.filter(d => d.id === 'GPS');
                 }
                 doneCallBack && doneCallBack(res);
             });
-            return { done: function(func){ doneCallBack = func; } };
+            return { done: function(func){ doneCallBack = func; } }
         }
     };
 
-    // 支持模块化导入
-    if(typeof module !== 'undefined' && module.exports){
-        module.exports.OneNetApi = OneNetApi;
-    } else {
-        global.OneNetApi = OneNetApi;
-    }
-
-})(window);
+    window.OneNetApi = OneNetApi;
+})();
